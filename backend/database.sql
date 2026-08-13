7f6e9a51c39753d9cae009d7f038580f4fb9f3fb
Endrew1223 <endrewhenriquelimaolivera@gmail.com>
2026-08-13 08:59:17 -0400

    Atualizando sistema para Flask

--- backend/database.sql
+++
@@ -1,9 +0,0 @@
--- Script de referência da estrutura de tabelas
-CREATE TABLE IF NOT EXISTS registromovimento (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    tipo TEXT NOT NULL,       -- 'colheita', 'venda' ou 'perda'
-    produto TEXT NOT NULL,    -- Ex: 'Alface Crespa'
-    quantidade REAL NOT NULL, -- Ex: 10.5
-    valor REAL DEFAULT 0,     -- Preço ou custo
-    data TEXT NOT NULL
-);
